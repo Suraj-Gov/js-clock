c4d0cbeca13bd3276638c3ce9eb546ce1f4dfd2b
Suraj Govind <surajgovind332000@gmail.com>
2020-05-06 19:40:52 +0530

    fixed -90deg bug

--- main.js
+++ main.js
@@ -8,15 +8,26 @@ setInterval(setTime, 1000);
 
 function setTime() {
   const time = new Date();
-  let hour = (time.getHours() % 12 * 30) - 90 + (time.getMinutes() * 0.5);
-  let minute = (time.getMinutes() * 6 ) - 90 + (time.getSeconds() * 0.1);
-  let second = (time.getSeconds() * 6 ) - 90;
+  let hour = (time.getHours() % 12) * 30 - 90 + time.getMinutes() * 0.5;
+  let minute = time.getMinutes() * 6 - 90 + time.getSeconds() * 0.1;
+  let second = time.getSeconds() * 6 - 90;
   hourHand.style.transform = `rotate(${hour}deg)`;
   minuteHand.style.transform = `rotate(${minute}deg)`;
   secondHand.style.transform = `rotate(${second}deg)`;
-  timeText.innerHTML = `${prepend(time.getHours()%12)}:${prepend(time.getMinutes())}:${prepend(time.getSeconds())}`;
+  timeText.innerHTML = `${prepend(time.getHours() % 12)}:${prepend(
+    time.getMinutes()
+  )}:${prepend(time.getSeconds())}`;
+  if (second == 264 || second == -90) {
+    secondHand.style.transition = "none";
+  } else secondHand.style.transition = "all 0.05s";
+  if (minute == 264 || minute == -90) {
+    minuteHand.style.transition = "none";
+  } else minuteHand.style.transition = "all 0.05s";
+  if (hour == 264 || hour == -90) {
+    hourHand.style.transition = "none";
+  } else hourHand.style.transition = "all 0.05s";
 }
 
 function prepend(num) {
-  return ( num < 10 ? '0' : '') + num;
+  return (num < 10 ? "0" : "") + num;
 }
